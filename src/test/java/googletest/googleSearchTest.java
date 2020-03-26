475dd4ac326ffeaed0cec67f284a6f654ff0cf0d
Konstantin <>
2020-03-26 10:20:59 +0300

    Allure

--- src/test/java/googletest/googleSearchTest.java
+++ src/test/java/googletest/googleSearchTest.java
@@ -1,11 +1,13 @@
 package googletest;
 
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 import pages.SearchPage;
 
 public class googleSearchTest extends BaseTest {
 
     @Test
+    @DisplayName("Google search test")
     public void testGoogleSearch() {
 
         webDriver.get("https://www.google.com/");
